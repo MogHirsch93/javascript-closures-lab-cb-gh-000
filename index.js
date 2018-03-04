1638904f96a28593a453465c07d5839694a54295
Morgan V Hirsch <morganhirsch93@hotmail.co.uk>
2018-03-04 14:22:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 function bumpCounter() {
   const counter = 0;
 
-  function addBump(counter){
+  return function addBump(counter){
     return counter + 1;
   }
-  function getBumps(counter){
+  return function getBumps(counter){
     return counter;
   }
-  return addBump(), getBumps();
 }
